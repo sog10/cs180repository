ba66d1147fcdeaa6dfb5bf37a6ca80bb1f121c57
sog10 <sog10@aber.ac.uk>
2013-11-24 15:10:12 +0000

    DictionaryEntry Class

--- DictionaryEntry.java
+++ DictionaryEntry.java
@@ -1,3 +1,21 @@
 public class DictionaryEntry {
-
+  private String pirateTranslation;
+  private String englishTranslation;
+  
+  public DictionaryEntry(String pirate, String english) {
+    pirateTranslation = pirate;
+    englishTranslation = english;
+  }
+  
+  public getEnglishTranslation() {
+    return englishTranslation;
+  }
+  
+  public getPirateTranslation() {
+    return pirateTranslation;
+  }
+  
+  public String toString() {
+    return pirateTranslation+" = "+englishTranslation;
+  }
 }
